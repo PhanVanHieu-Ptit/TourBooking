4f48971c0bb25f882ba84b8edeccd205bbd996f0
TuanHung <hungbuituan1@gmail.com>
2023-03-07 14:36:03 +0700

    data file

---
+++ BackEnd/Data.sql
@@ -0,0 +1,20 @@
+
+-- status
+INSERT INTO `status` VALUES (5,'Chờ cấp tài khoản','staff'),(1,'Chờ khởi hành ','tour'),(8,'Chờ xác nhận đặt','tourorder'),(4,'Đã hoàn thành','tour'),(2,'Đã hủy','tour'),(11,'Đã hủy','tourorder'),(7,'Đã khóa tài khoản','staff'),(3,'Đã khởi hành','tour'),(6,'Đang hoạt động','staff'),(12,'Đang sử dụng','tourorder'),(9,'Đặt thành công','tourorder'),(13,'Hoàn thành','tourorder'),(10,'Yêu cầu hủy','tourorder');
+
+
+
+-- //img
+insert into tourpicture(idTour,imageUrl) values(1,'https://ibb.co/WnT2JmJ');
+insert into tourpicture(idTour,imageUrl) values(1,'https://ibb.co/Zf2s3ws');
+insert into tourpicture(idTour,imageUrl) values(1,'https://ibb.co/4fMz0rv');
+insert into tourpicture(idTour,imageUrl) values(1,'https://ibb.co/SvH9WjV');
+insert into tourpicture(idTour,imageUrl) values(1,'https://ibb.co/mtznDvX');
+insert into tourpicture(idTour,imageUrl) values(1,'https://ibb.co/Z1t2hqb');
+
+"https://ibb.co/WnT2JmJ",
+"https://ibb.co/Zf2s3ws",
+"https://ibb.co/4fMz0rv",
+"https://ibb.co/SvH9WjV",
+"https://ibb.co/mtznDvX",
+"https://ibb.co/Z1t2hqb"
