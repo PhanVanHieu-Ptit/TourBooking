1d9b0d1d1a0f59099c7ed8bf4f560c5c361cd815
thuytrang10072001 <lethithuytrang20070805@gmail.com>
2023-03-26 08:42:22 +0700

    Load with page

--- FrontEnd/mobile/src/services/untils/index.js
+++ FrontEnd/mobile/src/services/untils/index.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
 
-const request = axios.create({
-    baseURL: 'http://192.168.1.23:3000',
+const request=axios.create({
+    baseURL: 'http://192.168.2.12:3000',
 
     responseType: 'json',
     withCredentials: true,
 });
 
-export const get = async (path, options = {}) => {
-    console.log('path: ', path);
-    const response = await request.get(path, options);
+export const get=async (path,options={}) => {
+    console.log('path: ',path);
+    const response=await request.get(path,options);
     return response.data;
 };
 
-export const post = async (path, options = {}) => {
+export const post=async (path,options={}) => {
     try {
-        const result = await request(path, {
+        const result=await request(path,{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             data: options,
         });
-        console.log('result: ', result);
+        console.log('result: ',result);
         return result;
     } catch (error) {
         console.log(error);
@@ -30,28 +30,28 @@ export const post = async (path, options = {}) => {
     }
 };
 
-export const postPrivate = async (path, data = {}, header = {}, method = 'POST') => {
+export const postPrivate=async (path,data={},header={},method='POST') => {
     try {
-        const result = await request(path, {
+        const result=await request(path,{
             method: method,
             headers: header,
             data: data,
         });
-        console.log('result: ', result);
+        console.log('result: ',result);
         return result;
     } catch (error) {
         console.log(error);
         return error.reponse.data;
     }
 };
-export const postImage = async (path, data = {}, header = {}, method = 'POST') => {
+export const postImage=async (path,data={},header={},method='POST') => {
     try {
-        const result = await request(path, {
+        const result=await request(path,{
             method: method,
             headers: header,
             data: data,
         });
-        console.log('result: ', result);
+        console.log('result: ',result);
         return result;
     } catch (error) {
         console.log(error);
